e31dfccb6d71103ef0fab86c95e789e3793459c7
Sirmuchai <sirmemu@gmail.com>
2023-02-20 16:54:34 +0300

    Kafka broker setup

---
+++ src/main/java/com/sity/mskafka/MsKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.sity.mskafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsKafkaApplication.class, args);
+    }
+
+}
